381116a284658fca3cdf895444f503e569049025
Miguel teheran <miguelteheran@gmail.com>
2018-06-12 23:13:55 -0500

    First version of the API

---
+++ Models/Rocket.cs
@@ -0,0 +1,10 @@
+namespace ApiNetcore.Models {
+
+    public class Rocket
+    {
+        public int Id {get;set;}
+        public string Builder {get;set;} 
+        public string Target {get;set;} 
+        public double Speed {get;set;} 
+    }
+}
